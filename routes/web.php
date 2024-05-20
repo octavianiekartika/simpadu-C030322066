ae3a903517dfedfc7bbaf8fb69cfe5b6e3713bfa
octavianiekartika <octavianie2003@gmail.com>
2024-05-20 10:52:30 +0800

    login, register, forgot password

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('pages.app.dashboard-simpadu', ['type_menu'=> ' ']);
+});
+
+Route::get('/login', function () {
+    return view('pages.auth.auth-login');
+})->name('login');
+
+Route::get('/register', function () {
+
+    return view('pages.auth.auth-register');
+})->name('register');
+
+Route::get('/forgot', function () {
+
+    return view('pages.auth.auth-forgot-password');
+})->name('forgot');
+
+Route::get('/reset', function () {
+
+    return view('pages.auth.auth-reset-password');
+})->name('reset');
